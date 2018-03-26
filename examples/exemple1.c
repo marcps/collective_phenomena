e4985f64f3dcae42fd7a588abc2c414e94e1393b
marcps <marc.ps.12@gmail.com>
2018-03-26 03:12:07 +0200

    update on first example

--- examples/exemple1.c
+++ examples/exemple1.c
@@ -2,19 +2,29 @@
 #include <stdio.h>
 #include <math.h>
 
+/*Aquest exercici s'ha dut a terme amb l'exemple següent:
+                p0(X) = (0,0.5,0.5,0)
+                W( X->Y ) =(0,   0.5 ,0.333,  0)
+                           (0.5,  0,  0.333,  0)
+                           (0.5, 0.5,  0,     1)
+                           (0,     0,  0.333, 0)
+Assegurant que la matriu és estacionaria*/
 int main(int argc, char const *argv[]) {
-  double v[4],g[4],p[4], W[4][4],temp;
+  int d;
+  printf("Nombre de dimensions. d=");
+  scanf("%d",&d);
+  double v[d],g[d],p[d], W[d][d],temp;
   int i,j,k,N; //Nombre de passes
   FILE *fp;
 
   N=1;
   //INPUT
-  for (i=0;i<4;i++){
+  for (i=0;i<d;i++){
     printf("p[%d]=",i+1 );
     scanf("%lf",&p[i]);
   }
-  for (i=0;i<4;i++){
-    for(j=0;j<4;j++){
+  for (i=0;i<d;i++){
+    for(j=0;j<d;j++){
       printf("W[%d][%d]=",i+1,j+1 );
       scanf("%lf",&W[i][j]);
     }
@@ -23,7 +33,7 @@ int main(int argc, char const *argv[]) {
   fp=fopen("resultats-ex1.txt","w");
   while(N<100){
 
-    for (i=0;i<4;i++){
+    for (i=0;i<d;i++){
       g[i]=p[i];
     }
     //------------------------------
@@ -32,15 +42,15 @@ int main(int argc, char const *argv[]) {
       //Ho posem tot a zero
       temp=0.0;
       //Multiplicació de matrius
-      for(i=0;i<4;i++){
+      for(i=0;i<d;i++){
         temp=0.0;
-        for(j=0;j<4;j++){
+        for(j=0;j<d;j++){
           temp += g[j]*W[i][j];
         }
         v[i]=temp;
       }
 
-      for (i=0;i<4;i++) {
+      for (i=0;i<d;i++) {
         g[i]=v[i];
       }
     }
@@ -48,7 +58,7 @@ int main(int argc, char const *argv[]) {
 
     //Escriurem els resultats a l'arxiu
     fprintf(fp,"%d ",N);
-    for(i=0;i<4;i++)
+    for(i=0;i<d;i++)
     {
       fprintf(fp,"%.10lf ",g[i]);
     }
