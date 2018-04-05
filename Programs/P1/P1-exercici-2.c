7ef32f0408e19f29cd26f12b7fb89c1c818cd4bd
marcps <marc.ps.12@gmail.com>
2018-04-05 20:57:16 +0200

    P1 Exercice 2

--- Programs/P1/P1-exercici-2.c
+++ Programs/P1/P1-exercici-2.c
@@ -5,3 +5,49 @@
 void rcarin_(int *,float *, int *);
 //SUBROUTINE RCARRY(RVEC,LENV)
 void rcarry_(float *, int *);
+
+void writeconfig(int L, int S[L][L]){
+  FILE *fp;
+  int i,j;
+  fp=fopen("P1-configuration.conf","w");
+  for(i=0;i<L;i++)
+  {
+    for(j=0;j<L;j++)
+    {
+      if(S[i][j]==1)
+      {
+        fprintf(fp,"%d %d\n",i,j);
+      }
+    }
+  }
+  fclose(fp);
+}
+
+int main(int argc, char const *argv[]){
+  int L=32,i,j,irand;
+  int seed;
+  printf("[*]Seed=");
+  scanf("%d",&seed);
+
+  int S[L][L];
+  int nrand=L*L*3+24;
+  float rrand[nrand];
+
+  //Generation of the estocastic vector:
+  rcarin_(&seed,rrand,&nrand);
+  rcarry_(rrand,&nrand);
+
+  //Generation of the spin matrix from the estocastic vector:
+  irand=1; //COUNTER
+  for(i=0;i<L;i++){
+    for(j=0;j<L;j++){
+      if(rrand[irand]<0.5){
+        S[i][j]=1;
+      }
+      else S[i][j]=-1;
+      irand += 1;
+    }
+  }
+  writeconfig(L,S);
+  return 0;
+}
