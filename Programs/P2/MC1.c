fc0149877212aeea66c3d1339249afb91e5f5bf8
marcps <marc.ps.12@gmail.com>
2018-04-12 21:57:01 +0200

    update P2

--- Programs/P2/MC1.c
+++ Programs/P2/MC1.c
@@ -1,5 +1,8 @@
+/*Marc Pascual - Universitat de Barcelona 2018 - Fenòmens Col·lectius i transicions de fase*/
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 
 //calls the Fortran SUBROUTINE RCARIN(IJKL,RVEC,LENV)
@@ -9,6 +12,18 @@ void rcarin_(int *,float *, int *);
 //SUBROUTINE RCARRY(RVEC,LENV)
 void rcarry_(float *, int *);
 
+double magne(int L, int S[L][L]){
+	int i,j;
+	double mag=0.0;
+	for(i=0;i<L;i++)
+	{
+		for(j=0;j<L;j++)
+		{
+			mag+=S[i][j];
+		}
+	}
+	return mag;
+}
 
 double energy(int L,int S[L][L],int pbc[L][2]){
 	double ene;
@@ -19,23 +34,26 @@ double energy(int L,int S[L][L],int pbc[L][2]){
 	{
 		for(j=0;j<L;j++)
 		{
-			ene -= S[i][j]*S[pbc[i][1]][j]+S[i][pbc[j][1]];
+			ene += -S[i][j]*S[pbc[i][1]][j]
+					-S[i][j]*S[i][pbc[j][1]];
 		}
 	}
 	return ene;
 }
 
-int main(void){
+int main(int argc, char const *argv[]){
 	// ----- VARIABLE DEFINITION ---------------
-	int L,i,j,k,irand;
-	printf("L=");
+	int L,i,j,k,p,irand,count;
+	printf("[*]L=");
 	scanf("%d",&L);
 	int nrand=L*L*3+24,seed;
-	printf("Seed=");
+	printf("[*]Seed=");
 	scanf("%d",&seed);
 	float rrand[nrand];
 	int pbc[L][2],S[L][L];
-	double ene;
+	double ene,magn,suma,de,vexp[4],temp;
+	printf("[*]Temperatura: temp=");
+	scanf("%lf",&temp);
 	// ------------------------------------------
 
 	// ----- SPIN MATRIX DEFINITION -------------
@@ -48,8 +66,8 @@ int main(void){
 	{
 		for(j=0;j<L;j++)
 		{
-			if(rrand[nrand]<0.5)S[i][j]=1;
-			else S[i][j]=-1;
+			if(rrand[irand]<0.5)S[i][j]=1;
+			else S[i][j]= -1;
 			irand+=1;
 		}
 	}
@@ -68,16 +86,49 @@ int main(void){
 
 	ene=energy(L,S,pbc);
 	printf("%lf\n",ene);
+	//------------------------------------------
 
 
 	// ----- MAIN LOOP BEGINS ------------------
 	rcarry_(rrand,&nrand);
 
-	
+	printf("Istep  M   E\n");
+	irand =0;
+	count=1;
+	for(i=0;i<L;i++)
+	{
+		for(j=0;j<L;j++)
+		{
+//This loop will test the change in energy of a change in spin
+//And reject it or not
+			k= (int)L*rrand[irand];
+			p=(int)L*rrand[irand+1];
+			irand+=2;
+			
+			suma=S[k][pbc[p][0]]+S[k][pbc[p][1]]+
+				S[pbc[k][0]][p]+S[pbc[k][1]][p];
+			
+			de=2.*S[k][p]*suma;
 
+			if(de<0) S[k][p]=-S[k][p];
+			else
+			{
+				irand+=1;
+				if(rrand[irand]<exp(-de/temp))
+				{
+					S[k][p]=-S[k][p];
+				}
+				//If not, the value is not accepted
+			}
+			ene=energy(L,S,pbc);
+			magn=magne(L,S);
 
-
-
+			//Now the results (STEP,MAGNETIZATION, ENERGY) will be printed
+			printf("%d     %2.lf      %2.lf\n",count,magn,ene);
+			count+=1;
+		}
+		
+	}
 
 	return 0;
 
